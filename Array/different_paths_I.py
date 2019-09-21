1da4de745eb60b3daa455b990acbb85b9a79b129
dreamhomes <2284672469@qq.com>
2019-09-21 21:45:25 +0800

    update

--- Array/different_paths_I.py
+++ Array/different_paths_I.py
@@ -6,10 +6,14 @@
 @File       : different_paths_I.py
 @Description: leetcode.62 左上角到右下角的不同路径数量。
 """
+import math
 
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # 排列组合
+        # return int(math.factorial(m+n-2)/math.factorial(m-1)/math.factorial(n-1))
+        # 动态规划
         dp = [[1] * n] + [[1] + [0] * (n - 1) for _ in range(m - 1)]
         for i in range(1, m):
             for j in range(1, n):
